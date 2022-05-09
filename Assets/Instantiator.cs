16948ae84844bdceec1390e0e6d3bcf0e196afd8
MicaelaCuan2 <micucuan@gmail.com>
2022-05-09 12:00:35 -0300

    Instantiator avanzado

---
+++ Assets/Instantiator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instantiator : MonoBehaviour
+{
+    public int numero;
+    public GameObject objectToClone;
+
+    public void CloneObject()
+    {
+        int num = 0;
+        while (num < numero)
+        {
+            Instantiate(objectToClone);
+            num++;
+        }
+    }
+}
